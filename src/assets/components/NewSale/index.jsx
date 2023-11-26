3c98786e0e6e24e20ad1346cc32555b47894ef5d
jfrlysona <mi7xxzdth@code.edu.az>
2023-11-26 05:47:52 +0400

    updated

--- src/assets/components/NewSale/index.jsx
+++ src/assets/components/NewSale/index.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
 function NewSale() {
   return (
-    <div className='newSale'>
-      <div className='newSaleLeft'>
-        <img width={759.5} height={660} src="https://dunker.qodeinteractive.com/wp-content/uploads/2022/12/h1-img-3.jpg" alt="" />
+    <div className="newSale">
+      <div className="newSaleLeft">
+        <img
+          width={759.5}
+          height={660}
+          src="https://dunker.qodeinteractive.com/wp-content/uploads/2022/12/h1-img-3.jpg"
+          alt=""
+        />
         <p>New</p>
       </div>
-      <div className='newSaleRight'>
-        <img width={759.5} height={660} src="https://dunker.qodeinteractive.com/wp-content/uploads/2022/12/h1-img-4.jpg" alt="" />
-        <div className='texts'>
-        <p className='p1'>Sale</p>
-        <p className='p2'>20% Off all Hoodies</p>
-        <button>SHOP NOW</button>
+      <div className="newSaleRight">
+        <img
+          width={759.5}
+          height={660}
+          src="https://dunker.qodeinteractive.com/wp-content/uploads/2022/12/h1-img-4.jpg"
+          alt=""
+        />
+        <div className="texts">
+          <p className="p1">Sale</p>
+          <p className="p2">20% Off all Hoodies</p>
+          <button>SHOP NOW</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default NewSale
+export default NewSale;
