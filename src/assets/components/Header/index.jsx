2300352b62957dc6fee8e24163c258e9b422e4b6
jfrlysona <mi7xxzdth@code.edu.az>
2023-11-27 05:34:01 +0400

    deleted carousel and added to develop

--- src/assets/components/Header/index.jsx
+++ src/assets/components/Header/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "../Navbar";
 import "./index.css";
-import CarouselFadeExample from "../CarouselSlide/";
 function Header() {
   return (
     <header>
       <Navbar />
-      <CarouselFadeExample/>
     </header>
   );
 }
